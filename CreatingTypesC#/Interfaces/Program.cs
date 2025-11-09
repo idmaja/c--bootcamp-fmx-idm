e359a6b4602dc6fbeb402283ec95ad1e2bd436b3
idmaja <dhimasm999@gmail.com>
2025-11-10 06:35:18 +0700

    learning interface

---
+++ CreatingTypesC#/Interfaces/Program.cs
@@ -0,0 +1,19 @@
+﻿IEnumerator e = new Countdown(); // Casts Countdown instance to IEnumerator interface
+while (e.MoveNext())
+    Console.Write(e.Current + " ");
+
+public interface IEnumerator // Interface Declaration
+{
+    bool MoveNext();      // Method
+    object Current { get; } // Read-only Property
+    void Reset();         // Method
+}
+
+internal class Countdown : IEnumerator // Implements the IEnumerator interface
+{
+    int count = 9;
+
+    public bool MoveNext() => count-- > 0; // Implementation of MoveNext
+    public object Current => count;        // Implementation of Current
+    public void Reset() { throw new NotSupportedException(); } // Implementation of Reset
+}
