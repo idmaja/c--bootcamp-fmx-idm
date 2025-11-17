60ead1c982de031dc9e2ccd82c08bdc01b2913d4
Dhimas <idhimas1@gmail.com>
2025-11-17 12:00:01 +0700

    Logic Exercise 3 (day 9)

--- LogicExercise1/Program.cs
+++ LogicExercise1/Program.cs
@@ -1,40 +1,29 @@
-﻿string? inputuser = Console.ReadLine();
-long panjang = string.IsNullOrWhiteSpace(inputuser) || !long.TryParse(inputuser, out long hasil) ? 10 : hasil;
+﻿using System.Text;
+
+Console.Write("Input panjang : ");
+string? inputuser = Console.ReadLine();
+long panjang = string.IsNullOrWhiteSpace(inputuser) || !long.TryParse(inputuser, out long hasil) ? 15 : hasil;
+
+var sbOutput = new StringBuilder();
 
 for (int i = 1; i < panjang + 1; i++)
 {
+    string output = "";
 
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-        if (i % 7 == 0)
-            Console.Write(", foobarjazz");
-        else
-            Console.Write(", foobar");
-    }
-    else if (i % 3 == 0)
-    {
-        if (i % 9 == 0)
-            Console.Write(", huzz");
-        else if (i % 7 == 0)
-            Console.Write(", foojazz");
-        else
-            Console.Write(", foo");
-    }
-    else if (i % 4 == 0)
-        Console.Write(", baz");
-    else if (i % 5 == 0)
-    {
-        if (i % 7 == 0)
-            Console.Write(", barjazz");
-        else
-            Console.Write(", bar");
-    }
-    else if (i % 7 == 0)
-        Console.Write(", jazz");
-    else if (i == 1)
-        Console.Write(i.ToString() + "");
-    else
-        Console.Write(", " + i.ToString());
+    if (i % 3 == 0) output += "foo";
+    if (i % 5 == 0) output += "bar";
+    if (i % 7 == 0) output += "jazz";
+    if (output == "") output = i.ToString();
+
+    if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) output = "foobarjazz";
+    else if (i % 3 == 0 && i % 5 == 0) output = "foobar";
+    else if (i % 3 == 0 && i % 9 == 0) output = "huzz";
+    else if (i % 3 == 0 && i % 7 == 0) output = "foojazz";
+    else if (i % 4 == 0) output = "baz";
+
+    if (i != 1) sbOutput.Append(", ");
+    sbOutput.Append(output);
 }
 
+Console.WriteLine(sbOutput.ToString());
 Console.ReadLine();
