2bda7d9226e8f1e0e553c6ea7092211effdcce58
idmaja <dhimasm999@gmail.com>
2025-11-23 00:13:12 +0700

    update: add ConditionalCompilation Project; refactor: learn and update Program.cs at ConditionalCompilation

---
+++ DiagnosticsCodeContracts/ConditionalCompilation/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// Conditional compilation membuat kode tertentu ikut dikompilasi hanya 
+/// jika simbol tertentu aktif. Simbol dicek saat compile. Kode yang tidak memenuhi 
+/// kondisi tidak pernah masuk ke hasil build.
+/// </summary>
+
+#define DEBUG
+
+#if TESTMODE
+        Console.WriteLine("Test mode aktif");
+#endif
+
+#if TESTMODE
+    Console.WriteLine("Test");
+#elif DEVMODE
+    Console.WriteLine("Dev");
+#else
+    Console.WriteLine("Normal");
+#endif
+
+Console.WriteLine("Hello, World!");
